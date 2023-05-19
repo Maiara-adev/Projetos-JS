a7f1db32482089ea460b10520aa293472afd1d2c
Maiara Almeida <127545597+Maiara-adev@users.noreply.github.com>
2023-05-19 01:30:37 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,40 +1,67 @@
-const mario = document.querySelector('.mario');
-const pipe = document.querySelector('.pipe');
+const pokemonName = document.querySelector('.pokemon-name');
+const pokemonNumber = document.querySelector('.pokemon-number');
+const pokemonImage = document.querySelector('.pokemon');
+const form = document.querySelector('.form');
+const input = document.querySelector('.input_search');
 
-const jump = () => {
-   mario.classList.add('jump');
+const buttonPrev = document.querySelector('.btn-prev');
+const buttonNext = document.querySelector('.btn-next');
 
-   setTimeout(() => {
-       mario.classList.remove('jump');
-   }, 500);
+let searchPokemon = 1;
+
+const fetchPokemon = async (pokemon) => {
+const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+if (APIResponse.status === 200) {
+
+    const data = await APIResponse.json();
+    return data;
 
 }
-  
+}
 
-const loop =setInterval(() => {
+const renderPokemon = async (pokemon) => {
 
-    const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    pokemonName.innerHTML = 'Loading...';
+    pokemonNumber.innerHTML = '';
+
+const data = await fetchPokemon(pokemon);
+
+if (data) {
+    pokemonImage.style.display = 'block';
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+    pokemonImage.src = data['sprites'] ['versions']['generation-v']['black-white']['animated']['front_default'];
     
+    input.value ='';
+    searchPokemon =data.id;
 
-    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+} else {
+    pokemonImage.style.display = 'none';
+    pokemonName.innerHTML = 'Not found :c';
+    pokemonNumber.innerHTML = '';
 
-        pipe.style.animation = 'none';
-        pipe.style.left= `${pipePosition}px`;
-        mario.style.animation = 'none';
-        mario.style.bottom=`${marioPosition}px`;
-        mario.src ='img/gameover.png';
-        mario.style.width = '75px'
-        mario.style.marginLeft = '50px'
-        clearInterval(loop);
+}
+}
+
+form.addEventListener('submit', (event) => {
+event.preventDefault();
+renderPokemon(input.value.toLowerCase());
+
+});
+
+buttonPrev.addEventListener('click', () => {
+    if(searchPokemon > 1){
+        searchPokemon -=1;
+        renderPokemon(searchPokemon);
     }
-       
-    
-}, 10);  
+   
+    });
 
+    buttonNext.addEventListener('click', () => {
+        searchPokemon +=1;
+        renderPokemon(searchPokemon);
+         });
+     
 
-document.addEventListener('keydown',jump)
-    if (event.code==='Space')  {
-        jump();
-    }
-
+renderPokemon(searchPokemon);
